b7f6a72f7db192c01de41e324b338fe79e150980
Oyefolu Toyyib Adeyemi <oyefolutoyyib16@gmail.com>
2024-07-10 20:17:05 +0100

    Completed Payment Page

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,17 +2,24 @@ import "./Button.css";
 import { useNavigate } from "react-router-dom";
 
 function Button({ className, link = "/", children, text }) {
+  const navigate = useNavigate();
+
+  if (children.includes("Payment")) {
+    return null;
+  }
   switch (text) {
     case "Go to Checkout":
       link = "/checkout";
       break;
-    case "Proceed to Payment":
-      link = "/";
+    case "Proceed to payment":
+      link = "/payment";
+      break;
+    case "Complete order":
+      link = "/successful";
       break;
     default:
       link = "/";
   }
-  const navigate = useNavigate();
   return (
     <button className={className} onClick={() => navigate(link)}>
       {children}
--- src/components/PaymentPage/PaymentPage.js
+++ src/components/PaymentPage/PaymentPage.js
@@ -1,67 +1,72 @@
-import "./CheckoutPage.css";
+import "./PaymentPage.css";
+import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleRight, faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 import RoutePageNav from "../RoutePageNav/RoutePageNav";
 import Footer from "../Footer/Footer";
 import OrderPage from "../OrderPage/OrderPage";
 import Button from "../Button/Button";
-import { useNavigate } from "react-router-dom";
 
-function CheckoutPage() {
+const PaymentPage = () => {
   const navigate = useNavigate();
   return (
-    <div className="checkout-page">
-      <div className="checkout-container">
+    <div className="payment-page">
+      <div className="payment-container">
         <RoutePageNav />
-        <div className="main-checkout">
+        <div className="main-payment">
           <span onClick={() => navigate("/")}>Home</span>
           <FontAwesomeIcon icon={faAngleRight} />
           <span onClick={() => navigate("/cart")}>Cart</span>
           <FontAwesomeIcon icon={faAngleRight} />
-          <span>Shipping</span>
+          <span onClick={() => navigate("/checkout")}>Shipping</span>
+          <FontAwesomeIcon icon={faAngleRight} />
+          <span>Payment</span>
 
-          <div className="checkout-box">
+          <div className="payment-box">
             <div className="box1 box expand-box">
-              <h2>Shipping Details</h2>
+              <h2>Payment Details</h2>
               <form className="formic">
-                <div className="address">
-                  <p>Use saved address</p>
+                <div className="saved-card">
+                  <p>Use saved card</p>
                   <select name="" id="" disabled>
-                    <option value="123, Lagos Ikeja">123, Lagos Ikeja</option>
+                    <option value="123, Lagos Ikeja">
+                      Mastercard ending 725
+                    </option>
                   </select>
                 </div>
 
-                <p>First line of address</p>
+                <p>Name on card</p>
                 <div className="input">
-                  <input type="text" placeholder="123" />
-                  <FontAwesomeIcon icon={faCircleCheck} />
-                </div>
-                <p>Street name</p>
-                <div className="input">
-                  <input type="text" placeholder="Medical Road" />
+                  <input type="text" placeholder="Code Wizard" />
                   <FontAwesomeIcon icon={faCircleCheck} />
                 </div>
 
-                <div className="postcode">
+                <p>Card Number</p>
+                <div className="input">
+                  <input type="text" placeholder="123 - 456 -" />
+                </div>
+
+                <div className="expiration">
                   <div>
-                    <p>Postcode</p>
-                    <input type="text" placeholder="ABC - 123" />
+                    <p>Expiration</p>
+                    <div className="expired-date">
+                      <input type="text" placeholder="03" />
+                      <input type="text" placeholder="01" className="end" />
+                    </div>
                   </div>
                   <div>
-                    <p>Select Shipping</p>
-                    <select name="" id="">
-                      <option value="Select Shipping">Select Shipping</option>
-                    </select>
+                    <p>CVC</p>
+                    <input type="text" placeholder="123" />
                   </div>
                 </div>
               </form>
               <div className="btns">
                 <p>Cancel order</p>
-                <Button className={"done"}>Done</Button>
+                <Button className={"complete-order"}>Complete order</Button>
               </div>
             </div>
             <OrderPage
-              child="Proceed to payment"
+              child="Payment"
               subTotal={760}
               deliveryFee={15}
               total={775}
@@ -72,6 +77,6 @@ function CheckoutPage() {
       </div>
     </div>
   );
-}
+};
 
-export default CheckoutPage;
+export default PaymentPage;
